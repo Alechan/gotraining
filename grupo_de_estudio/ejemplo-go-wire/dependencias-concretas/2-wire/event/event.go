09a81cb6a87e7d9e1a94c33a080842a050edb0b8
Alechan <alejandro.danos@mercadolibre.com>
2022-05-27 13:01:18 -0300

    gde: fix wrong commit of dependencias-concretas/2-wire

--- grupo_de_estudio/ejemplo-go-wire/dependencias-concretas/2-wire/event/event.go
+++ grupo_de_estudio/ejemplo-go-wire/dependencias-concretas/2-wire/event/event.go
@@ -2,21 +2,17 @@ package event
 
 import (
 	"fmt"
-	"github.com/ardanlabs/gotraining/grupo_de_estudio/ejemplo-go-wire/dependencias-concretas/2-wire/message"
+	"github.com/ardanlabs/gotraining/grupo_de_estudio/ejemplo-go-wire/dependencias-concretas/2-wire/greeter"
 )
 
 // NewEvent creates an event with the specified greeter.
-type iGreeter interface {
-	Greet() message.Message
-}
-
-func NewEvent(g iGreeter) Event {
+func NewEvent(g greeter.Greeter) Event {
 	return Event{Greeter: g}
 }
 
 // Event is a gathering with greeters.
 type Event struct {
-	Greeter iGreeter
+	Greeter greeter.Greeter
 }
 
 // Start ensures the event starts with greeting all guests.
